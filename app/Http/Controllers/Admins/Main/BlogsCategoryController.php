15e407ff0f44cb310e637c6aec41b5506a0abdd7
aliomid <aomid5182@gmail.com>
2021-01-04 14:39:31 +0330

    hi2

--- app/Http/Controllers/Admins/Main/BlogsCategoryController.php
+++ app/Http/Controllers/Admins/Main/BlogsCategoryController.php
@@ -5,25 +5,22 @@ namespace App\Http\Controllers\Admins\Main;
 use App\Http\Controllers\Controller;
 use App\lib\File\ImageUploader;
 use App\lib\Messages\FlashMessage;
-use App\Models\Category;
+use App\Models\BlogsCategory;
 use App\Models\Image;
 use Illuminate\Http\Request;
 
-class CategoryController extends Controller
+class BlogsCategoryController extends Controller
 {
 
     public function create()
     {
-        return view('Admins.Subject.Category.create');
+        return view('Admins.Blogs.Category.create');
     }
     public function store(Request $request)
     {
         if ($request->title) {
-            $category = Category::create($request->except(['_token', 'image']));
+            $category = BlogsCategory::create($request->except(['_token', 'image']));
             if ($category) {
-                if ($request->image) {
-                    Image::create(['url' => ImageUploader::upload($request->image, 'Category/'), 'item_id' => $category->id, 'type' => 'category']);
-                }
                 FlashMessage::set('success', 'دسته ثبت شد');
                 return back();
             } else {
@@ -39,9 +36,9 @@ class CategoryController extends Controller
 
     public function edit($id)
     {
-        $category = Category::find($id);
+        $category = BlogsCategory::find($id);
         if ($category) {
-            return view('Admins.Subject.Category.edit', compact('category'));
+            return view('Admins.Blogs.Category.edit', compact('category'));
         } else {
             FlashMessage::set('error', 'درخواست کامل نبود');
             return back();
@@ -52,17 +49,9 @@ class CategoryController extends Controller
     public function update(Request $request)
     {
         if ($request->id) {
-            $category = Category::find($request->id);
-            $image = Image::where('item_id', $category->id)->where('type', 'category')->first();
+            $category = BlogsCategory::find($request->id);
             if ($category) {
-                if ($request->image) {
-                    if ($image) {
-                        $image->update(['url' => ImageUploader::upload($request->image, 'Category/', null, $image->url)]);
-                    } else {
-                        Image::create(['url' => ImageUploader::upload($request->image, 'Category/'), 'item_id' => $category->id, 'type' => 'category']);
-                    }
-                }
-                $category->update($request->except(['_token','_method','image']));
+                $category->update($request->except(['_token', '_method']));
                 FlashMessage::set('success', 'دسته ویرایش شد');
                 return back();
             } else {
@@ -78,14 +67,12 @@ class CategoryController extends Controller
     public function destroy(Request $request)
     {
         if ($request->id) {
-             $category = Category::find($request->id);
-             if ($category) {
-
-             }else{
+            $category = BlogsCategory::find($request->id);
+            if ($category) {
+            } else {
                 FlashMessage::set('error', 'درخواست کامل نبود');
                 return back();
-             }
-
+            }
         } else {
             FlashMessage::set('error', 'درخواست کامل نبود');
             return back();
