c467d1c6d4fd819e584c7547d06d658139977f10
aliomid1 <aomid5182@gmail.com>
2021-01-06 17:17:34 +0330

    reserve3

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -12,27 +12,27 @@ class Conversation extends Model
 
     public function User()
     {
-        return $this->hasOne('App\User' , 'id' , 'user_id');
+        return $this->hasOne('App\User', 'id', 'user_id');
     }
 
 
     public function Advisor()
     {
-        return $this->hasOne('App\Models\Advisors' , 'id' , 'advisor_id');
+        return $this->hasOne(Advisors::class, 'id', 'advisor_id');
     }
 
 
 
     public function Subject()
     {
-        return $this->hasOne('App\Models\Subject' , 'id' , 'subject_id');
+        return $this->hasOne(Subject::class, 'id', 'subject_id');
     }
 
 
 
     public function UserImage()
     {
-         $user_id_image =  $this->hasOne('\App\Models\Image' , 'item_id' , 'user_id');
-         return $user_id_image->where('type' , 'profile_user');
+        $user_id_image =  $this->hasOne(Image::class, 'item_id', 'user_id');
+        return $user_id_image->where('type', 'profile_user');
     }
 }
--- resources/views/Users/Main/NowAdviced.blade.php
+++ resources/views/Users/Main/NowAdviced.blade.php
@@ -1,5 +1,5 @@
 @extends('layout.Users.template')
-@section('title','داشبورد')
+@section('title','تاریخچه مشاوره ها')
 
 @section('style')
 
@@ -34,21 +34,7 @@
 
                     <div class="d-flex justify-content-between">
                         <div class="input-group mb-3">
-                            {{-- <div class="input-group-prepend">
-                                <div class="input-group-text">
-                                    <div class="custom-control custom-checkbox">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck">
-                                        <label class="custom-control-label" for="customCheck"></label>
-                                    </div>
-                                </div>
-                                <button class="btn btn-light dropdown-toggle" type="button" data-toggle="dropdown"
-                                    aria-haspopup="true" aria-expanded="false">اقدامات
-                                </button>
-                                <div class="dropdown-menu dropdown-menu-right">
-                                    <a class="dropdown-item" href="#">حذف </a>
-                                    <a class="dropdown-item" href="#">علامت خوانده شده</a>
-                                </div>
-                            </div> --}}
+                            <h5>نوبت های انلاین</h5>
                         </div>
                     </div>
 
@@ -65,14 +51,14 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    @forelse($User_Conversation as $item)
+                                    @forelse($User_Conversation->where('type','online') as $item)
                                         <tr>
                                             <td>
                                                 {{ $item->Advisor ? $item->Advisor->name : "کاربر حذف شده" }}
                                             </td>
                                             <td>
                                                 <div class="">
-                                                    {{ $item->Subject ? $item->Subject->title : "موضوع حذف شده است" }}
+                                                    {{ $item->subject  }}
                                                 </div>
                                             </td>
                                             <td>
@@ -117,6 +103,149 @@
         </div>
     </div>
 
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+
+                    <div class="d-flex justify-content-between">
+                        <div class="input-group mb-3">
+                            <h5>نوبت های تلفنی</h5>
+                        </div>
+                    </div>
+
+                    <div class="table-email-list">
+                        <div class="table-responsive" tabindex="1" style=" outline: none;">
+                            <table class="table table-hover text-center">
+                                <thead>
+                                    <tr>
+                                        <th>نام مشاور</th>
+                                        <th>توضیحات</th>
+                                        <th>وضعیت</th>
+                                        <th>زمان انجام مشاوره</th>
+
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @forelse($User_Conversation->where('type','in') as $item)
+                                        <tr>
+                                            <td>
+                                                {{ $item->Advisor ? $item->Advisor->name : "کاربر حذف شده" }}
+                                            </td>
+                                            <td>
+                                                <div class="">
+                                                    {{ $item->subject  }}
+                                                </div>
+                                            </td>
+                                            <td>
+                                                @if($item->status == "done")
+                                                    <span class="text-success">انجام شده</span>
+                                                @endif
+                                                @if($item->status == "doing")
+                                                    <span class="text-secondary">در حال انجام...</span>
+                                                @endif
+                                            </td>
+                                            <td>
+                                                {{ $item->start_at ? \Morilog\Jalali\Jalalian::forge($item->start_at)->format('Y/m/d - H:i:s') : '-' }}
+                                            </td>
+                                        </tr>
+                                    @empty
+                                        <tr>
+                                            <td colspan="4" class="text-center">موردی یافت نشد</td>
+                                        </tr>
+                                    @endforelse
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                    <nav class="m-t-30">
+                        <ul class="pagination justify-content-center">
+
+                            {{ $User_Conversation->links() }}
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+
+                    <div class="d-flex justify-content-between">
+                        <div class="input-group mb-3">
+                            <h5>نوبت های حضوری</h5>
+                        </div>
+                    </div>
+
+                    <div class="table-email-list">
+                        <div class="table-responsive" tabindex="1" style=" outline: none;">
+                            <table class="table table-hover text-center">
+                                <thead>
+                                    <tr>
+                                        <th>نام مشاور</th>
+                                        <th>توضیحات</th>
+                                        <th>وضعیت</th>
+                                        <th>زمان انجام مشاوره</th>
+
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @forelse($User_Conversation->where('type','out') as $item)
+                                        <tr>
+                                            <td>
+                                                {{ $item->Advisor ? $item->Advisor->name : "کاربر حذف شده" }}
+                                            </td>
+                                            <td>
+                                                <div class="">
+                                                    {{ $item->subject  }}
+                                                </div>
+                                            </td>
+                                            <td>
+                                                @if($item->status == "done")
+                                                    <span class="text-success">انجام شده</span>
+                                                @endif
+                                                @if($item->status == "doing")
+                                                    <span class="text-secondary">در حال انجام...</span>
+                                                @endif
+                                            </td>
+                                            <td>
+                                                {{ $item->start_at ? \Morilog\Jalali\Jalalian::forge($item->start_at)->format('Y/m/d - H:i:s') : '-' }}
+                                            </td>
+                                        </tr>
+                                    @empty
+                                        <tr>
+                                            <td colspan="4" class="text-center">موردی یافت نشد</td>
+                                        </tr>
+                                    @endforelse
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+
+                    <nav class="m-t-30">
+                        <ul class="pagination justify-content-center">
+                            {{-- <li class="page-item disabled">
+                                <a class="page-link" href="#" tabindex="-1" aria-disabled="true">قبلی</a>
+                            </li>
+                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">بعدی</a>
+                            </li> --}}
+                            {{ $User_Conversation->links() }}
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </div>
 @endsection
 
